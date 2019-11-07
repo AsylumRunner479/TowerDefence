8e60f07768720f94cbaa1cf5dba4b6634145b2f4
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-07 15:24:17 +1100

    added money indicator

--- test/Galatic Dividers/Assets/Scripts/Shop/Shop.cs
+++ test/Galatic Dividers/Assets/Scripts/Shop/Shop.cs
@@ -11,12 +11,15 @@ public class Shop : MonoBehaviour
     void Start()
     {
         money = 100;
-        GUI.Button(new Rect(scr.x * 14.25f, scr.y *  0.5f), scr.x * 2, scr.y * 0.5f), "Money: " + money);
+        
     }
 
     // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        scr = new Vector2(Screen.width / 16, Screen.height / 9);
+        GUI.Button(new Rect(scr.x * 14.25f, scr.y * 0.5f, scr.x * 2, scr.y * 0.5f), "Money: " + money);
+        //DisplayCustom();
+        //DisplayStats();
     }
 }
