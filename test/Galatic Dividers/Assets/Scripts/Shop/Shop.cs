24654a0459dd394be5b485565bf5a6851b68de01
AsylumRunner479 <54158974+AsylumRunner479@users.noreply.github.com>
2019-11-07 15:13:03 +1100

    adding gui

--- test/Galatic Dividers/Assets/Scripts/Shop/Shop.cs
+++ test/Galatic Dividers/Assets/Scripts/Shop/Shop.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Shop : MonoBehaviour
 {
+    public Vector2 scr;
     public static float money;
     // Start is called before the first frame update
     void Start()
     {
         money = 100;
+        GUI.Button(new Rect(scr.x * 14.25f, scr.y *  0.5f), scr.x * 2, scr.y * 0.5f), "Money: " + money);
     }
 
     // Update is called once per frame
